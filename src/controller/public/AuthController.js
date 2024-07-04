1a68dde1728851e1927787f375144fe5059c80c9
enrick-dev <enricksantos03@gmail.com>
2024-07-03 22:24:35 -0300

    ajustes do merge

--- src/controller/public/AuthController.js
+++ src/controller/public/AuthController.js
@@ -1,30 +1,33 @@
-import md5 from "md5"
-import jwt from 'jsonwebtoken'
-import { db } from "../../BD/BDConect.js"
+import md5 from 'md5';
+import jwt from 'jsonwebtoken';
+import { db } from '../../BD/BDConect.js';
 
-export class AuthController {
-  async authenticate(req, res) {
-    const { email, password } = req.body
-    const isValuePassword = md5(password)
-    const qUser = `
+const getUserByEmail = (email, callback) => {
+  const qUser = `
     SELECT id, email, password
     FROM users_business 
     WHERE users_business.email = ?
     LIMIT 1
-    `
+  `;
+  db.query(qUser, [email], callback);
+};
 
-    if (!email) return res.status(403).json({ error: 'Insira um E-mail' })
-    if (!password) return res.status(403).json({ error: 'Insira uma senha' })
+export class AuthController {
+  async authenticate(req, res) {
+    const { email, password } = req.body;
 
-    db.query(qUser, [email], (err, data) => {
-      if (err) return res.json({ error: 'erro interno ao processar requisição', errorDetails: err })
-      if (!data[0]) return res.status(403).json({ error: 'Email não cadastrado' })
-      if (data[0].password != isValuePassword) return res.status(400).json({ error: 'Senha incorreta' })
-      return res.status(200).json({
-        id: data[0].id,
-        email,
-        token: jwt.sign({ id: data[0].id }, process.env.SECRET_TOKEN, { expiresIn: '4h' })
-      })
-    })
+    if (!email) return res.status(403).json({ error: 'Insira um E-mail' });
+    if (!password) return res.status(403).json({ error: 'Insira uma senha' });
+
+    getUserByEmail(email, (err, data) => {
+      if (err) return res.status(500).json({ error: 'Erro interno ao processar requisição', errorDetails: err });
+      if (!data[0]) return res.status(403).json({ error: 'Email não cadastrado' });
+
+      const isValuePassword = md5(password);
+      if (data[0].password != isValuePassword) return res.status(400).json({ error: 'Senha incorreta' });
+
+      const token = jwt.sign({ id: data[0].id }, process.env.SECRET_TOKEN, { expiresIn: '4h' });
+      return res.status(200).json({ id: data[0].id, email, token });
+    });
   }
 }
