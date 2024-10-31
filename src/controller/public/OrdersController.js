050450775606f15239fcae957a7649d7616ec678
Enrick Santos <enricksantos03@gmail.com>
2024-10-30 21:22:26 -0300

    refactor: alterado rota de pedidos

--- src/controller/public/OrdersController.js
+++ src/controller/public/OrdersController.js
@@ -1,20 +1,43 @@
 import { db } from '../../BD/BDConect.js';
 
 const getOrdersByBusiness = (getOrder, callback) => {
-  const { bussinessID, bussinessName, active } = getOrder;
+  const { bussinessID, bussinessName, active, combo } = getOrder;
 
-  const q = `
-    SELECT a.id, a.id_business, a.name, a.description, a.price, a.photo_thumb, a.active, a.category, c.name as category_name
+  let q = `
+    SELECT a.id, a.id_business, a.name, a.description, a.price, a.photo_thumb, a.active, a.combo, a.category, c.name as category_name
     FROM orders_business a
     INNER JOIN users_business b ON a.id_business = b.id 
     INNER JOIN orders_business_category c ON a.category = c.id 
-    WHERE ${bussinessID ? "b.id  = ?" : "b.name_url = ?"} ${typeof active !== 'undefined' ? 'AND a.active = ' + active : ''}
+    WHERE 1=1
   `;
-  db.query(q, [bussinessID || bussinessName], callback);
+  const params = [];
+
+  if (bussinessID) {
+    q += ' AND b.id = ?';
+    params.push(bussinessID);
+  }
+
+  if (bussinessName) {
+    q += ' AND b.name_url = ?';
+    params.push(bussinessName);
+  }
+
+  if (combo) {
+    q += ' AND a.combo = ?';
+    params.push(Boolean(combo));
+  }
+
+  if (typeof active !== 'undefined') {
+    q += ' AND a.active = ?';
+    params.push(Boolean(active));
+  }
+
+  db.query(q, params, callback);
 };
 
 const createOrder = (orderData, callback) => {
-  const { bussinessID, name, description, price, category, photo_thumb } = orderData;
+  const { bussinessID, name, description, price, category, photo_thumb } =
+    orderData;
   const q = `
     INSERT INTO orders_business (id_business, name, description, price, category, photo_thumb)
     VALUES (?, ?, ?, ?, ?, ?)
@@ -23,9 +46,9 @@ const createOrder = (orderData, callback) => {
   db.query(q, values, callback);
 };
 
-
 const updateOrder = (id, updatedData, callback) => {
-  const { name, description, price, category, photo_thumb, active } = updatedData;
+  const { name, description, price, category, photo_thumb, active, combo } =
+    updatedData;
   let query = 'UPDATE orders_business SET';
   const values = [];
 
@@ -47,6 +70,11 @@ const updateOrder = (id, updatedData, callback) => {
     values.push(active);
   }
 
+  if (typeof combo !== 'undefined') {
+    query += ' combo = ?,';
+    values.push(combo);
+  }
+
   if (category) {
     query += ' category = ?,';
     values.push(category);
@@ -63,7 +91,6 @@ const updateOrder = (id, updatedData, callback) => {
   db.query(query, values, callback);
 };
 
-
 const findRolesCategory = (callback) => {
   const q = `
     SELECT id, name FROM orders_business_category;
@@ -73,28 +100,49 @@ const findRolesCategory = (callback) => {
 
 export class OrdersController {
   async getOrders(req, res) {
-    const { bussinessID, bussinessName } = req.query;
-
-    if (!bussinessID && !bussinessName) return res.status(400).json({ error: 'informação base não foi enviada' });
-
     getOrdersByBusiness(req.query, (err, data) => {
-      if (err) return res.status(500).json({ error: 'Erro interno ao processar requisição', errorDetails: err });
-      if (!data[0]) return res.status(404).json({ error: 'Loja não possui itens' });
+      if (err)
+        return res
+          .status(500)
+          .json({
+            error: 'Erro interno ao processar requisição',
+            errorDetails: err,
+          });
+      if (!data[0])
+        return res.status(404).json({ error: 'Loja não possui itens' });
 
       return res.status(200).json(data);
     });
   }
 
   async createOrder(req, res) {
-    const { bussinessID, name, description, price, category, photo_thumb } = req.body;
+    const { bussinessID, name, description, price, category, photo_thumb } =
+      req.body;
 
-    if (!bussinessID || !name || !price || !category || !description || !photo_thumb) {
-      return res.status(400).json({ error: 'Dados incompletos para criação do pedido' });
+    if (
+      !bussinessID ||
+      !name ||
+      !price ||
+      !category ||
+      !description ||
+      !photo_thumb
+    ) {
+      return res
+        .status(400)
+        .json({ error: 'Dados incompletos para criação do pedido' });
     }
 
     createOrder(req.body, (err, result) => {
-      if (err) return res.status(500).json({ error: 'Erro ao criar pedido', errorDetails: err });
-      return res.status(200).json({ message: 'Pedido criado com sucesso', orderId: result.insertId });
+      if (err)
+        return res
+          .status(500)
+          .json({ error: 'Erro ao criar pedido', errorDetails: err });
+      return res
+        .status(200)
+        .json({
+          message: 'Pedido criado com sucesso',
+          orderId: result.insertId,
+        });
     });
   }
 
@@ -103,11 +151,16 @@ export class OrdersController {
     const updatedData = req.body;
 
     if (Object.keys(req.body).length === 0) {
-      return res.status(400).json({ error: 'Nenhuma informação foi enviada para atualizar' });
+      return res
+        .status(400)
+        .json({ error: 'Nenhuma informação foi enviada para atualizar' });
     }
 
     updateOrder(id, updatedData, (err, result) => {
-      if (err) return res.status(500).json({ error: 'Erro ao atualizar pedido', errorDetails: err });
+      if (err)
+        return res
+          .status(500)
+          .json({ error: 'Erro ao atualizar pedido', errorDetails: err });
 
       return res.status(200).json({ message: 'Pedido atualizado com sucesso' });
     });
@@ -115,7 +168,10 @@ export class OrdersController {
 
   async findRolesCategory(_, res) {
     findRolesCategory((err, result) => {
-      if (err) return res.status(500).json({ error: 'Erro ao atualizar pedido', errorDetails: err });
+      if (err)
+        return res
+          .status(500)
+          .json({ error: 'Erro ao atualizar pedido', errorDetails: err });
 
       return res.status(200).json(result);
     });
